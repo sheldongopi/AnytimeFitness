1d7c1912482858e6ec4320565dc1d5b8be822235
LINM04 <m.j.vd.Linden@rocmondriaan.nl>
2021-09-27 12:21:19 +0200

    initial commit

---
+++ Templates/registreren.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <html>
+    <?php
+    include_once('defaults/head.php');
+    ?>
+    <body>
+        <div class="container">
+            <?php
+            include_once ('defaults/header.php');
+            include_once ('defaults/menu.php');
+            include_once ('defaults/pictures.php');
+            ?>
+            <h4>registreren</h4>
+            Hier komt de registratie pagina
+            <hr>
+            <?php
+            include_once ('defaults/footer.php');
+            ?>
+        </div>
+    </body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+//require '../Modules/Categories.php';
+//require '../Modules/Products.php';
+// require '../Modules/Database.php';
+
+$request = $_SERVER['REQUEST_URI'];
+$params = explode("/", $request);
+$title = "Anytime Fitness";
+$titleSuffix = "";
+
+switch ($params[1]) {
+    case 'home':
+        $titleSuffix = ' | Home';
+        include_once "../Templates/home.php";
+        break;
+    case 'categories':
+        $titleSuffix = ' | Categories';
+        include_once "../Templates/categories.php";
+        break;
+    case 'registreren':
+        $titleSuffix = ' | Registreren';
+        include_once "../Templates/registreren.php";
+        break;
+    default:
+        $titleSuffix = ' | Home';
+        include_once "../Templates/home.php";
+}
+
+function getTitle() {
+    global $title, $titleSuffix;
+    return $title . $titleSuffix;
+}
